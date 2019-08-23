6fb6c756ffe67935b78b2cccf72463e916a24bdc
Davi Cruz <47984117+davicruzof@users.noreply.github.com>
2019-08-23 14:29:45 -0300

    Delete cores.h

--- cores.h
+++
@@ -1,7 +0,0 @@
-#define preto textcolor(BLACK)
-#define branco textcolor(WHITE)
-#define azul textcolor(LIGHTCYAN)
-#define vermelho textcolor(LIGHTRED)
-#define bpreto textbackground(BLACK)
-#define bbranco textbackground(WHITE)
-#define nulo cout << '\0'
